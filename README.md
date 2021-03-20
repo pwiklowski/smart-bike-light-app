ebee7522f9777804647b459068f8c815aea3cb78
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2021-03-20 09:02:32 +0100

    Add material ui

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Credits
+
+<div>Icons made by <a href="" title="mangsaabguru">mangsaabguru</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,15 @@ import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
 import { CurrentStateComponent } from './current-state/current-state.component';
 import { OnBoardingComponent } from './on-boarding/on-boarding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SettingsComponent,
-    CurrentStateComponent,
-    OnBoardingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SettingsComponent, CurrentStateComponent, OnBoardingComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatIconModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

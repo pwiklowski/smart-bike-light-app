cad31f6df430a4f6fa3fc13fba671b0fe5464494
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2021-03-20 08:35:38 +0100

    Make app skeleton from generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CurrentStateComponent } from './current-state/current-state.component';
+import { OnBoardingComponent } from './on-boarding/on-boarding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    CurrentStateComponent,
+    OnBoardingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/current-state/current-state.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-state',
+  templateUrl: './current-state.component.html',
+  styleUrls: ['./current-state.component.scss']
+})
+export class CurrentStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

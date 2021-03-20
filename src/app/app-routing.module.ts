01058713650deb913c9fe9cef642a3ad12484a41
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2021-03-20 08:36:43 +0100

    Configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { SettingsComponent } from './settings/settings.component';
+import { CurrentStateComponent } from './current-state/current-state.component';
+import { OnBoardingComponent } from './on-boarding/on-boarding.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'state', component: CurrentStateComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', component: OnBoardingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

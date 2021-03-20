46e94570eb5efc53a3c796f095e5183853299dd2
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2021-03-20 15:27:13 +0100

    Add initial version of settings view

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,33 @@
+import { Router } from '@angular/router';
+import { BleService, LightAnimation } from './../ble.service';
 import { Component, OnInit } from '@angular/core';
+import { EnumValues } from 'enum-values';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  animations: string[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bleService: BleService, private router: Router) {
+    this.animations = EnumValues.getNames(LightAnimation);
   }
 
+  ngOnInit(): void {}
+
+  onFrontAnimationChanged($event) {
+    console.log($event);
+    this.bleService.setFrontLightAnimation($event.value);
+  }
+
+  onBackAnimationChanged($event) {
+    console.log($event);
+    this.bleService.setBackLightAnimation($event.value);
+  }
+
+  openCurrentState() {
+    this.router.navigate(['/state']);
+  }
 }
